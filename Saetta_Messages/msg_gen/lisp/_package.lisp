f8f3c198b01bd1341959747ba93a89cbb71d1fa5
clacarbone <cla_carbone@tiscali.it>
2013-02-18 17:23:20 +0100

    simplified folder names

--- Saetta_Messages/msg_gen/lisp/_package.lisp
+++
@@ -1,9 +0,0 @@
-(cl:defpackage Saetta_Messages-msg
-  (:use )
-  (:export
-   "<CMD_VEL>"
-   "CMD_VEL"
-   "<ODOM>"
-   "ODOM"
-  ))
-
--- Saetta_Messages/msg_gen/lisp/_package_cmd_vel.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package Saetta_Messages-msg)
-(cl:export '(LINEAR-VAL
-          LINEAR
-          ANGULAR-VAL
-          ANGULAR
-))
