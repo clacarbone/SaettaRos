aa6038037c2973df9440f64d0393a055a34a6364
clacarbone <cla_carbone@tiscali.it>
2013-02-19 01:22:06 +0100

    parameter fetching now working correctly

--- saetta_vision/main.cpp
+++ saetta_vision/main.cpp
@@ -19,17 +19,12 @@
 
 int main ( int argc, char** argv )
 {
+bool configured=false;
     std::string visionPrefix, visionRobnum, robName, visionRobCtrl;
     std::stringstream ss;
     std::vector<std::pair<std::string, int>> robVector;
     int robNum;
-    Vision::VisionConfig myconfig;
-    myconfig.set_camera ( 1280, 720 );
-    myconfig.set_windows ( 640, 480 );
-    myconfig.set_mapname ( "Map.txt" );
-    myconfig.set_showwindows ( true );
-    Vision::Vision myvision ( myconfig );
-    myvision.Start ( );
+
 
     ros::init ( argc, argv, "Saetta_Vision" );
     ros::NodeHandle n;
@@ -37,22 +32,8 @@ int main ( int argc, char** argv )
     // Declare variables that can be modified by launch file or command line.
     int rate = 20;
 
-    n.getParam("saetta_vision_prefix", visionPrefix);
-    n.getParam("saetta_vision_robnum", visionRobnum);
-    n.getParam("saetta_vision_ctrl_topic", visionRobCtrl);
-    robNum = atoi(visionRobnum.c_str());
-    for (int i=0; i<robNum;i++)
-    {
-        ss.clear ();
-        ss << visionPrefix <<robNum;
-        robName = ss.str();
-        if (n.hasParam(robName))
-        {
-            std::pair<std::string,int> localpair(robName,-1);
-            robVector.push_back (localpair);            
-        }
-    }
-    
+
+
     //    localbase->InitWifi(ip);
     // Initialize node parameters from launch file or command line.
     // Use a private node handle so that multiple instances of the node can be run simultaneously
@@ -72,13 +53,50 @@ int main ( int argc, char** argv )
 
     // Tell ROS how fast to run this node.
     ros::Rate r ( rate );
+    Vision::VisionConfig myconfig;
+    myconfig.set_camera ( 1280, 720 );
+    myconfig.set_windows ( 640, 480 );
+    myconfig.set_mapname ( "Map.txt" );
+    myconfig.set_showwindows ( true );
+    Vision::Vision myvision ( myconfig );
+    myvision.Start ( );    
     while (n.ok ( ))
     {
+if(!configured)
+{
+    n.getParam("saetta_vision_prefix", visionPrefix);
+std::cout << "Prefix: " << visionPrefix << std::endl;
+    n.getParam("saetta_vision_robnum", visionRobnum);
+
+    n.getParam("saetta_vision_cmd_topic", visionRobCtrl);
+std::cout << "Ctrl Topic: " << visionRobCtrl << std::endl;
+    if(robNum = atoi(visionRobnum.c_str()) == 0)
+robNum=10;
+std::cout << "RobNum: " << robNum << std::endl;    
+for (int i=0; i<robNum;i++)
+    {
+	robName="";
+        ss.str("");
+        ss << visionPrefix <<(i+1);
+        robName = ss.str();
+	std::cout << "Looking up " << ss.str() << std::endl;
+        if (n.hasParam(robName))
+        {
+            std::pair<std::string,int> localpair(robName,-1);
+	    std::cout << "Found robot " << robName << std::endl;
+            robVector.push_back (localpair);            
+        }
+    }
+
+
+
+configured=true;
+}
         ros::spinOnce ( );
         r.sleep ( );
     }
 
-    myvision.Stop ( );
+    //myvision.Stop ( );
 
 
 }
