e5424611e71122f1f8912324ffd9bd829e1445e4
clacarbone <cla_carbone@tiscali.it>
2013-03-06 12:06:23 +0100

    new module for algorithms, currently implementing rigid formation control

---
+++ saetta_algorithms/include/rigidform.hpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   Vision.hpp
+ * Author: attilio
+ *
+ * Created on February 15, 2013, 6:06 PM
+ */
+
+#ifndef RIGIDFORM_HPP
+#define	RIGIDFORM_HPP
+
+//#define __GXX_EXPERIMENTAL_CXX0X__
+#include <thread>
+#include <mutex>
+#include <cstdlib>
+#include <cstdio>
+#include <sstream>
+#include <iostream>
+#include <cctype>
+#include <algorithm>
+#include <string>
+#include <boost/algorithm/string.hpp>
+#include <ros/ros.h>
+#include <saetta_msgs/cmd_vel.h>
+#include <math.h>
+#include <tf/transform_listener.h>
+namespace rigidform
+{
+
+
+};
+#endif	/* VISION_HPP */
+
---
+++ saetta_algorithms/src/rigidform.cpp
@@ -0,0 +1,165 @@
+#define __GXX_EXPERIMENTAL_CXX0X__
+#include "rigidform.hpp"
+#define LOOP_RATE	50 	// Hz
+#define	MAX_SPEED	0.2	// m/s
+#define PIVOT_D_AXIS	0.08	// m
+
+bool publish = false;
+
+void timerCallback(const ros::TimerEvent&)
+{
+    publish = true;
+}
+
+template <typename Type1, typename Type2>
+int RosFetchParam( ros::NodeHandle& node, Type1& parmname, Type2 storage )
+{
+    Type2 temp;
+    if (node.getParam(parmname, temp))
+    {
+        storage = temp;
+        return 0;
+    }
+    std::string newstr(parmname);
+    boost::to_upper(newstr);
+    ROS_ERROR("Parameter \"%s\" does not exist. Check your configuration.", newstr.c_str());
+    return -1;
+
+}
+
+template <typename Type1>
+bool VectorContains( std::vector<Type1> vect, Type1 elem )
+{
+    if (std::find(vect.begin(), vect.end(), elem) != vect.end()) return true;
+    else return false;
+}
+
+
+int main( int argc, char** argv )
+{
+    
+
+    ros::init(argc, argv, "Saetta_RigidForm");
+    ros::NodeHandle n;
+
+    // Initialize node parameters from launch file or command line.
+    // Use a private node handle so that multiple instances of the node can be run simultaneously
+    // while using different parameters.
+    ros::NodeHandle base_handle_("~");
+
+    //base_handle_.param("rate", rate, int(40));
+    //ros::Subscriber sub = n.subscribe ( "/saetta/velocity", 10, &Saetta_Base::listenerCallback, localbase );
+    //ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+
+
+    // Tell ROS how fast to run this node.
+
+    ros::Rate r(LOOP_RATE);
+    ros::Publisher pubCmdVel1, pubCmdVel2, pubCmdVel3;
+    pubCmdVel1 = n.advertise<saetta_msgs::cmd_vel > ("/robot_1/cmd_vel", 1);
+    pubCmdVel2 = n.advertise<saetta_msgs::cmd_vel > ("/robot_2/cmd_vel", 1);
+    pubCmdVel3 = n.advertise<saetta_msgs::cmd_vel > ("/robot_3/cmd_vel", 1);
+    tf::TransformListener listener;
+    tf::StampedTransform transform1, transform2, transform3;
+    tf::Vector3 vactual1, vactual2, vactual3, cmd1, cmd2, cmd3, *tmp1, *tmp2, *err;
+    tf::Quaternion qactual1,qactual2,qactual3;
+    double roll,pitch,yaw, dist12, dist13, dist23, factor, lin_vel, ang_vel;
+    saetta_msgs::cmd_vel msg;
+    //ros::Timer timer = n.createTimer(ros::Duration(0.25), timerCallback);
+    while (n.ok())
+    {        
+	
+    	try{
+      	    listener.lookupTransform("world", "/robot_1/base_link", ros::Time(0), transform1);
+      	    listener.lookupTransform("world", "/robot_2/base_link", ros::Time(0), transform2);
+      	    listener.lookupTransform("world", "/robot_3/base_link", ros::Time(0), transform3);
+	    vactual1 = transform1.getOrigin();
+	    vactual2 = transform2.getOrigin();
+	    vactual3 = transform3.getOrigin();
+	    qactual1 = transform1.getRotation();
+	    qactual2 = transform2.getRotation();
+	    qactual3 = transform3.getRotation();
+	    
+	    dist12 = (powf(vactual2.x()-vactual1.x(),2)+powf(vactual2.y()-vactual1.y(),2));
+	    dist13 = (powf(vactual3.x()-vactual1.x(),2)+powf(vactual3.y()-vactual1.y(),2));
+	    dist23 = (powf(vactual3.x()-vactual2.x(),2)+powf(vactual3.y()-vactual2.y(),2));
+
+	    //compute error correction for node 1
+	    err = new tf::Vector3(vactual2.x() - vactual1.x(),vactual2.y() - vactual1.y(), 0);
+	    factor = - 0.5 * (dist12 - 1.0);
+	    tmp1 = new tf::Vector3(factor * err->x(), factor * err->y(), 0);
+	    err = new tf::Vector3(vactual3.x() - vactual1.x(), vactual3.y() - vactual1.y(), 0);
+	    factor = - 0.5 * (dist13 - 1.0);
+	    tmp2 = new tf::Vector3(factor * err->x(), factor * err->y(), 0);
+	    cmd1 = *tmp1 + *tmp2;
+	    tf::Matrix3x3(qactual1).getRPY(roll, pitch, yaw);
+	    lin_vel=cos(yaw) *cmd1.x()+sin(yaw)*cmd1.y();
+	    ang_vel=-( 1/PIVOT_D_AXIS )*sin(yaw) *cmd1.x()+(1/PIVOT_D_AXIS)*cos(yaw)*cmd1.y();
+	    if(publish)
+	    {
+		msg.linear = lin_vel * 100;
+		if (msg.linear > 5.0)
+		    msg.linear = 5.0;
+		msg.angular = ang_vel;
+		pubCmdVel1.publish(msg);
+	    }
+	
+	    //compute error correction for node 2
+	    err = new tf::Vector3(vactual1.x() - vactual2.x(), vactual1.y() - vactual2.y(), 0);
+	    factor = - 0.5 * (dist12 - 1.0);
+	    tmp1 = new tf::Vector3(factor * err->x(), factor * err->y(), 0);
+	    err = new tf::Vector3(vactual3.x() - vactual2.x(), vactual3.y() - vactual2.y(), 0);
+	    factor = - 0.5 * (dist23 - 1.0);
+	    tmp2 = new tf::Vector3(factor * err->x(), factor * err->y(), 0);
+	    cmd2 = *tmp1 + *tmp2;
+	    tf::Matrix3x3(qactual1).getRPY(roll, pitch, yaw);
+	    lin_vel=cos(yaw) *cmd2.x()+sin(yaw)*cmd2.y();
+	    ang_vel=-( 1/PIVOT_D_AXIS )*sin(yaw) *cmd2.x()+(1/PIVOT_D_AXIS)*cos(yaw)*cmd2.y();
+	    if(publish)
+	    {
+		msg.linear = lin_vel * 100;
+		if (msg.linear > 5.0)
+		    msg.linear = 5.0;
+		msg.angular = ang_vel;
+		pubCmdVel2.publish(msg);
+	    }
+
+	    //compute error correction for node 3
+	    err = new tf::Vector3(vactual2.x() - vactual3.x(), vactual2.y() - vactual3.y(), 0);
+	    factor = - 0.5 * (dist23 - 1.0); 
+	    tmp1 = new tf::Vector3(factor * err->x(), factor * err->y(), 0);
+	    err = new tf::Vector3(vactual1.x() - vactual3.x(), vactual1.y() - vactual3.y(), 0);
+	    factor = - 0.5 * (dist13 - 1.0);
+	    tmp2 = new tf::Vector3(factor * err->x(), factor * err->y(), 0);
+	    cmd3 = *tmp1 + *tmp2;
+	    tf::Matrix3x3(qactual1).getRPY(roll, pitch, yaw);
+	    lin_vel=cos(yaw) *cmd3.x()+sin(yaw)*cmd3.y();
+	    ang_vel=-( 1/PIVOT_D_AXIS )*sin(yaw) *cmd3.x()+(1/PIVOT_D_AXIS)*cos(yaw)*cmd3.y();
+	    if(publish)
+	    {
+		msg.linear = lin_vel * 100;
+		if (msg.linear > 5.0)
+		    msg.linear = 5.0;
+		msg.angular = ang_vel;
+		pubCmdVel3.publish(msg);
+		publish = false;
+	    }
+	}
+	catch (tf::TransformException ex){
+      	    ROS_ERROR("%s",ex.what());
+    	}
+	tf::Matrix3x3(qactual1).getRPY(roll, pitch, yaw);
+	std::cout << "R1 Px: " << vactual1.x() << " Py: " << vactual1.y() << "0: " << yaw << std::endl;
+	std::cout << "Robot 1 -> X: " << cmd1.x() << "  Y: " << cmd1.y() << std::endl;
+	std::cout << "Robot 2 -> X: " << cmd2.x() << "  Y: " << cmd2.y() << std::endl;
+	std::cout << "Robot 3 -> X: " << cmd3.x() << "  Y: " << cmd3.y() << std::endl;
+	std::cout << "Formation : [" << dist12 << " , " << dist13 << " , " << dist23 << "]" << std::endl << std::endl;
+        ros::spinOnce();
+        r.sleep();
+    }
+
+
+
+}
+
+
