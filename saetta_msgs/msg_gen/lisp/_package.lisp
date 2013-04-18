23d4175371af5b117fd1bd734dc1029376bd1add
clacarbone <cla_carbone@tiscali.it>
2013-04-18 16:00:29 +0200

    removed unnecessary build files from saetta_msgs

--- saetta_msgs/msg_gen/lisp/_package.lisp
+++
@@ -1,9 +0,0 @@
-(cl:defpackage saetta_msgs-msg
-  (:use )
-  (:export
-   "<CMD_VEL>"
-   "CMD_VEL"
-   "<ODOM>"
-   "ODOM"
-  ))
-
--- saetta_msgs/msg_gen/lisp/_package_cmd_vel.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package saetta_msgs-msg)
-(cl:export '(LINEAR-VAL
-          LINEAR
-          ANGULAR-VAL
-          ANGULAR
-))
