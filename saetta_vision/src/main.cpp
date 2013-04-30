b9bd34f2f5d3aaaac295d6522557790cc3d13c34
clacarbone <cla_carbone@tiscali.it>
2013-04-30 18:21:34 +0200

    added cam launch

--- saetta_vision/src/main.cpp
+++ saetta_vision/src/main.cpp
@@ -21,6 +21,7 @@
 
 #define LOOP_RATE	50 //Hz
 #define FILTER_COEFF	0.8
+
 /*
  * 
  */
@@ -49,7 +50,7 @@ typedef struct
 } autoconf_status_t;
 
 template <typename Type1, typename Type2>
-int RosFetchParam( ros::NodeHandle& node, Type1& parmname, Type2 storage )
+int RosFetchParam(ros::NodeHandle& node, Type1& parmname, Type2 storage)
 {
     Type2 temp;
     if (node.getParam(parmname, temp))
@@ -65,17 +66,17 @@ int RosFetchParam( ros::NodeHandle& node, Type1& parmname, Type2 storage )
 }
 
 template <typename Type1>
-bool VectorContains( std::vector<Type1> vect, Type1 elem )
+bool VectorContains(std::vector<Type1> vect, Type1 elem)
 {
     if (std::find(vect.begin(), vect.end(), elem) != vect.end()) return true;
     else return false;
 }
-bool robothasmoved( int k, saetta_vision::RobotList_t list1, saetta_vision::RobotList_t list2 );
-void ConfigInit( autoconf_status_t& obj, unsigned int sec1, unsigned int sec2);
-void ConfigSetPause ( autoconf_status_t& obj, unsigned int secs);
-void filterRobList ( saetta_vision::RobotList_t oldlist, saetta_vision::RobotList_t newlist);
+bool robothasmoved(int k, saetta_vision::RobotList_t list1, saetta_vision::RobotList_t list2);
+void ConfigInit(autoconf_status_t& obj, unsigned int sec1, unsigned int sec2);
+void ConfigSetPause(autoconf_status_t& obj, unsigned int secs);
+void filterRobList(saetta_vision::RobotList_t oldlist, saetta_vision::RobotList_t newlist);
 
-int main( int argc, char** argv )
+int main(int argc, char** argv)
 {
     std::vector<int> indexVector;
     saetta_vision::RobotList_t robotListLocal, movedRobotListLocal, robotListOld;
@@ -109,15 +110,15 @@ int main( int argc, char** argv )
     myconfig.set_mapname("Map.txt");
     myconfig.set_showwindows(true);
     saetta_vision::Vision myvision(myconfig);
-    if(myvision.Start()!= 0)
-	return -1;
+    if (myvision.Start() != 0)
+        return -1;
     autoConfiguration.state = autoconf_states_t::init;
     ros::Rate r(LOOP_RATE);
     ros::Publisher pubCmdVel;
     tf::TransformBroadcaster br;
     tf::Transform transform;
     float distx, disty, dist;
-    
+
     while (n.ok())
     {
 
@@ -151,10 +152,10 @@ int main( int argc, char** argv )
                         break;
                     }
                     std::cout << "Ctrl Topic: " << autoConfiguration.visionRobCtrl << std::endl << "Holding off until vision stabilizes..." << std::endl << std::endl;
-                    
+
                     autoConfiguration.state = autoconf_states_t::pausing;
                     autoConfiguration.next_state = autoconf_states_t::move;
-                    
+
                     break;
 
                 case move:
@@ -172,17 +173,17 @@ int main( int argc, char** argv )
                     topic = ss.str();
                     pubCmdVel = n.advertise<saetta_msgs::cmd_vel > (topic, 1);
                     autoConfiguration.state = pausing;
-		    autoConfiguration.next_state = sendmv;
-		    ConfigSetPause(autoConfiguration, 2);
+                    autoConfiguration.next_state = sendmv;
+                    ConfigSetPause(autoConfiguration, 2);
                     std::cout << "Waiting for movement... " << std::endl;
                     break;
 
                 case err:
                     break;
 
-		case sendmv:
-		    pubCmdVel.publish(vel);
-		    autoConfiguration.state = wait;
+                case sendmv:
+                    pubCmdVel.publish(vel);
+                    autoConfiguration.state = wait;
                     break;
 
                 case wait:
@@ -193,7 +194,7 @@ int main( int argc, char** argv )
                         autoConfiguration.delayCounter = 0;
                     }
                     break;
-                    
+
                 case pausing:
                     autoConfiguration.pauseCounter++;
                     if (autoConfiguration.pauseCounter >= autoConfiguration.pauseTsrh)
@@ -266,21 +267,21 @@ int main( int argc, char** argv )
         if ((autoConfiguration.state == configured) && (myvision.getTryRobList(robotListLocal)) == true)
         {
             //robotListLocal = myvision.getRobList();
-	    filterRobList(robotListOld, robotListLocal);
-		std::cout << std::endl;
+            filterRobList(robotListOld, robotListLocal);
+            std::cout << std::endl;
             for (std::vector < std::pair < std::string, int >> ::iterator myiter = robVector.begin(); myiter != robVector.end(); ++myiter)
             {
                 ss.str("");
                 ss << "/" << (*myiter).first << "/base_link";
                 int index = (*myiter).second;
-                transform.setOrigin(tf::Vector3(robotListLocal.robList[index].coord.x/1000, robotListLocal.robList[index].coord.y/1000, 0));
-                transform.setRotation(tf::Quaternion(0, 0, robotListLocal.robList[index].orientation/360*2*M_PI));
-                br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world",ss.str()));
-		std::cout << (*myiter).first << "  X: " <<  robotListLocal.robList[index].coord.x << " Y: " << robotListLocal.robList[index].coord.y << " Cnt " <<  robotListLocal.robList[index].center.x << ":" <<  robotListLocal.robList[index].center.y << std::endl;
+                transform.setOrigin(tf::Vector3(robotListLocal.robList[index].coord.x / 1000, robotListLocal.robList[index].coord.y / 1000, 0));
+                transform.setRotation(tf::Quaternion(0, 0, robotListLocal.robList[index].orientation / 360 * 2 * M_PI));
+                br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", ss.str()));
+                std::cout << (*myiter).first << "  X: " << robotListLocal.robList[index].coord.x << " Y: " << robotListLocal.robList[index].coord.y << " Cnt " << robotListLocal.robList[index].center.x << ":" << robotListLocal.robList[index].center.y << std::endl;
                 //std::cout << ss.str() << "\ttf_trasl(" << robotListLocal.robList[index].coord.x/1000 <<"," << robotListLocal.robList[index].coord.y/1000 << "," << 0 << ")" << std::endl;
                 //std::cout << ss.str() << "\ttf_rot(" << 0 <<","<<  0 <<"," << robotListLocal.robList[index].orientation/360*2*M_PI << ")" << std::endl;
             }
-	    robotListOld = robotListLocal;
+            robotListOld = robotListLocal;
         }
         ros::spinOnce();
         r.sleep();
@@ -291,7 +292,7 @@ int main( int argc, char** argv )
 
 }
 
-void ConfigInit( autoconf_status_t& obj ,unsigned int sec1, unsigned int sec2)
+void ConfigInit(autoconf_status_t& obj, unsigned int sec1, unsigned int sec2)
 {
     obj.state = init;
     obj.index = 0;
@@ -299,18 +300,18 @@ void ConfigInit( autoconf_status_t& obj ,unsigned int sec1, unsigned int sec2)
     obj.visionRobnum = 5;
     obj.delayCounter = 0;
     obj.delayTsrh = sec1 * LOOP_RATE;
-    obj.pauseCounter=0;
+    obj.pauseCounter = 0;
     obj.pauseTsrh = sec2 * LOOP_RATE;
 }
 
-void ConfigSetPause ( autoconf_status_t& obj, unsigned int secs)
+void ConfigSetPause(autoconf_status_t& obj, unsigned int secs)
 {
-   obj.pauseCounter=0;
-   obj.pauseTsrh = secs * LOOP_RATE;
+    obj.pauseCounter = 0;
+    obj.pauseTsrh = secs * LOOP_RATE;
 
 }
 
-bool robothasmoved( int k, saetta_vision::RobotList_t list1, saetta_vision::RobotList_t list2 )
+bool robothasmoved(int k, saetta_vision::RobotList_t list1, saetta_vision::RobotList_t list2)
 {
     float distx, disty, dist;
     distx = powf(list1.robList[k].coord.x - list2.robList[k].coord.x, 2);
@@ -322,14 +323,14 @@ bool robothasmoved( int k, saetta_vision::RobotList_t list1, saetta_vision::Robo
 
 }
 
-void filterRobList ( saetta_vision::RobotList_t oldlist, saetta_vision::RobotList_t newlist)
+void filterRobList(saetta_vision::RobotList_t oldlist, saetta_vision::RobotList_t newlist)
 {
     //for (std::vector < std::pair < std::string, int >> ::iterator myiter = robVector.begin(); myiter != robVector.end(); ++myiter)
-    for (int index = 0; index<newlist.robNum; index++)
+    for (int index = 0; index < newlist.robNum; index++)
     {
-	//int index = (*myiter).second;
-	newlist.robList[index].coord.x = (1-FILTER_COEFF) * newlist.robList[index].coord.x + FILTER_COEFF * oldlist.robList[index].coord.x;
-	newlist.robList[index].coord.y = (1-FILTER_COEFF) * newlist.robList[index].coord.y + FILTER_COEFF * oldlist.robList[index].coord.y;
-	newlist.robList[index].orientation = (1-FILTER_COEFF) * newlist.robList[index].orientation + FILTER_COEFF * oldlist.robList[index].orientation;
+        //int index = (*myiter).second;
+        newlist.robList[index].coord.x = (1 - FILTER_COEFF) * newlist.robList[index].coord.x + FILTER_COEFF * oldlist.robList[index].coord.x;
+        newlist.robList[index].coord.y = (1 - FILTER_COEFF) * newlist.robList[index].coord.y + FILTER_COEFF * oldlist.robList[index].coord.y;
+        newlist.robList[index].orientation = (1 - FILTER_COEFF) * newlist.robList[index].orientation + FILTER_COEFF * oldlist.robList[index].orientation;
     }
 }
